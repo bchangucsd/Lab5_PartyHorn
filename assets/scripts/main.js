64b35739855e92b826717f829fe5ece6e9b5e19d
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-02-03 15:57:19 -0800

    change volume icon based on volume

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,7 @@ let honkBtn = document.getElementById("honk-btn");
 let numberInput = document.getElementById("volume-number");
 let hornSound = document.getElementById("horn-sound");
 let slider = document.getElementById("volume-slider");
+let volumeIcon = document.getElementById("volume-image");
 
 // input field changes audio volume and slider
 numberInput.addEventListener("input", numberUpdateVolume);
@@ -28,10 +29,27 @@ function cancelSubmission(event) {
 function numberUpdateVolume() {
     hornSound.volume = numberInput.value / 100;
     slider.value = numberInput.value;
+    updateIcon();
 }
 
 // function for updating audio volume from slider
 function sliderUpdateVolume() {
     hornSound.volume = slider.value / 100;
     numberInput.value = slider.value;
+    updateIcon();
+}
+
+function updateIcon() {
+    if (slider.value >= 67) {
+        volumeIcon.src = "./assets/media/icons/volume-level-3.svg";
+    }
+    else if (slider.value >= 34) {
+        volumeIcon.src = "./assets/media/icons/volume-level-2.svg";
+    }
+    else if (slider.value >= 1) {
+        volumeIcon.src = "./assets/media/icons/volume-level-1.svg";
+    }
+    else {
+        volumeIcon.src = "./assets/media/icons/volume-level-0.svg";
+    }
 }
