1769290ca84ef9bf63080e89374fac6af7725288
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-02-03 14:57:41 -0800

    button click plays sound

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,13 @@
 // main.js
 
 // TODO
+document.getElementById("honk-btn").addEventListener("click", cancelSubmission);
+document.getElementById("honk-btn").addEventListener("click", honk);
+
+function honk() {
+    document.getElementById("horn-sound").play();
+}
+
+function cancelSubmission(event) {
+    event.preventDefault();
+}
